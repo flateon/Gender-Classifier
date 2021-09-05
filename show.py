2b4ae4a62b5cc0c1c34c7a8494154c125a3cda44
ZCH <1220569030@qq.com>
2021-09-05 18:47:54 +0800

    Add show_pr_curve Add show_model

--- show.py
+++ show.py
@@ -1,20 +1,23 @@
 import torch
 from matplotlib import pyplot as plt
+from torch import nn
 from torch.utils.data import DataLoader
 
 from model import get_model
+from torch.utils.tensorboard import SummaryWriter
+
 from my_dataset import MyDataset
 from setting import *
 
 
-def show_example(model, _dataloader):
+def show_example(model_, dataloader_):
     figure = plt.figure(dpi=600)
     cols, rows = 15, 7
     cnt = 1
     with torch.no_grad():
-        for images, labels in _dataloader:
+        for images, labels in dataloader_:
             images, labels = images.to('cuda'), labels.to('cuda')
-            predict_labels = model(images)
+            predict_labels = model_(images)
             predicts = (predict_labels > 0).float()
             for image, label, predict in zip(images, labels, predicts):
                 figure.add_subplot(rows, cols, cnt)
@@ -30,14 +33,14 @@ def show_example(model, _dataloader):
     plt.show()
 
 
-def show_error(model, _dataloader):
+def show_error(model_, dataloader_):
     figure = plt.figure(dpi=600)
     cols, rows = 6, 4
     cnt = 1
     with torch.no_grad():
-        for images, labels in _dataloader:
+        for images, labels in dataloader_:
             images, labels = images.to('cuda'), labels.to('cuda')
-            predict_labels = model(images)
+            predict_labels = model_(images)
             predicts = (predict_labels > 0).float().squeeze()
             for idx in (predicts != labels).nonzero():
                 figure.add_subplot(rows, cols, cnt)
@@ -55,11 +58,11 @@ def show_error(model, _dataloader):
     plt.show()
 
 
-def draw_scatter_plot(model, _dataloader):
+def draw_scatter_plot(model_, dataloader_):
     figure = plt.figure(dpi=600)
     with torch.no_grad():
-        for images, labels in _dataloader:
-            predict_labels = model(images.to('cuda')).cpu()
+        for images, labels in dataloader_:
+            predict_labels = model_(images.to('cuda')).cpu()
             colors = ['g' if x else 'r' for x in (predict_labels > 0).float().squeeze(1) == labels]
             plt.scatter(predict_labels[:, 0], predict_labels[:, 1], s=0.2, c=colors)
     plt.xlim((-10, 10))
@@ -68,21 +71,45 @@ def draw_scatter_plot(model, _dataloader):
     plt.show()
 
 
-def draw_hist(model, _dataloader):
+def draw_hist(model_, dataloader_):
     figure = plt.figure(dpi=600)
     with torch.no_grad():
-        for images, labels in _dataloader:
-            predict_labels = model(images.to('cuda')).cpu()
+        for images, labels in dataloader_:
+            predict_labels = model_(images.to('cuda')).cpu()
             plt.hist(predict_labels.squeeze().numpy(), bins=80, density=True, color='b')
     plt.savefig('hist.png', dpi=600)
     plt.show()
 
 
+def show_pr_curve(model_, dataloader_, writer_):
+    all_labels = []
+    all_predict = []
+    with torch.no_grad():
+        for images, labels in dataloader_:
+            all_predict.append((nn.Sigmoid()(model_(images.to('cuda')))).cpu())
+            all_labels.append(labels)
+    all_predict = torch.cat(all_predict).squeeze()
+    all_labels = torch.cat(all_labels) == 1
+    writer_.add_pr_curve('pr_curve', all_labels, all_predict, 0)
+    writer_.close()
+
+
+def show_model(model_, dataset_, writer_):
+    images, _ = next(iter(dataset_))
+    model_.to('cpu')
+    writer_.add_graph(model_, images.unsqueeze(0))
+    model_.to('cuda')
+    writer_.close()
+
+
 if __name__ == '__main__':
+    writer = SummaryWriter('./runs/test')
     dataset = MyDataset(img_dir=TESTING_DATASET_PATH)
     dataloader = DataLoader(dataset, batch_size=250, num_workers=4, pin_memory=True)
     model = get_model().eval().to('cuda')
-    model.load_state_dict(torch.load('model/resnet34_0.990000_08-15_18-41.pkl'))
+    model.load_state_dict(torch.load('model/resnet34_0.994131_08-29_20-25.pkl'))
+    show_model(model, dataset, writer)
     show_example(model, dataloader)
+    show_pr_curve(model, dataloader, writer)
     show_error(model, dataloader)
     draw_hist(model, dataloader)
